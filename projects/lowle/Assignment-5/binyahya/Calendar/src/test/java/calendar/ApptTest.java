7f45ee6d0213004150ed47e66a606928812af153
Lowle <lowle@oregonstate.edu>
2018-06-03 21:35:14 -0700

    Updated test cases

--- projects/lowle/Assignment-5/binyahya/Calendar/src/test/java/calendar/ApptTest.java
+++ projects/lowle/Assignment-5/binyahya/Calendar/src/test/java/calendar/ApptTest.java
@@ -15,7 +15,7 @@ public class ApptTest {
 		String string0 = appt0.toString();
 		assertEquals(2, appt0.getRecurBy());
 		assertFalse(appt0.isRecurring());
-		assertEquals("\t14/9/2018 at 3:30pm ,Birthday Party, This is my birthday party\n", string0);
+		assertEquals("\t9/14/2018 at 3:30pm ,Birthday Party, This is my birthday party\n", string0);
 		assertEquals(0, appt0.getRecurIncrement());
 		appt0.setValid();
 	}
@@ -24,7 +24,7 @@ public class ApptTest {
 	public void testInvalidUpperMinute() throws Throwable {
 		Appt appt0 = new Appt(15, 61, 9, 14, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
 		String string0 = appt0.toString();
-		assertEquals("\t14/9/2018 at 3:61pm ,Birthday Party, This is my birthday party\n", string0);
+		assertEquals("\t9/14/2018 at 3:61pm ,Birthday Party, This is my birthday party\n", string0);
 		appt0.setValid();
 	}
 
@@ -239,7 +239,7 @@ public class ApptTest {
 		String string0 = appt0.toString();
 		assertEquals(2, appt0.getRecurBy());
 		assertFalse(appt0.isRecurring());
-		assertEquals("\t14/9/2018 at 12:30pm ,Birthday Party, This is my birthday party\n", string0);
+		assertEquals("\t9/14/2018 at 12:30pm ,Birthday Party, This is my birthday party\n", string0);
 		assertEquals(0, appt0.getRecurIncrement());
 		appt0.setValid();
 	}
@@ -250,7 +250,7 @@ public class ApptTest {
 		String string0 = appt0.toString();
 		assertEquals(2, appt0.getRecurBy());
 		assertFalse(appt0.isRecurring());
-		assertEquals("\t14/9/2018 at 11:30am ,Birthday Party, This is my birthday party\n", string0);
+		assertEquals("\t9/14/2018 at 11:30am ,Birthday Party, This is my birthday party\n", string0);
 		assertEquals(0, appt0.getRecurIncrement());
 		appt0.setValid();
 	}
